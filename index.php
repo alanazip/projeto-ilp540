932165767f836fe52f2e898e2211acd23e68fab3
Alana Nascimento de Souza <74723979+alanazip@users.noreply.github.com>
2022-03-07 17:30:02 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,33 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-br">        
+
     <head>
+        <title>login</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="style.css">
         <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
 
-        <title>login</title>
     </head>
     <body>
-        <div class="balao"><br>faça login ou cadastre-se<br> para entrar</div>
+        <div class="balao" style="align-items: center;"><br>faça login ou cadastre-se<br> para entrar</div>
         <img src="https://i0.wp.com/fazerfestas.com/wp-content/uploads/2016/12/nova-turma-da-moranguinho-gatinho-pudim-new-strawberry-shortcake-02.png?resize=236%2C289&ssl=1" alt="pudim" class="pudim">
+        <br>
+        <div id="login" align="center">
+            <form action="#" method="post">
+                <label class="label" for="text">login: <input type="text" name="usuario" id="usuario" class="login"></label><br><br>
+                <label class="label" for="password">senha: <input type="password" name="senha" id="senha" class="senha"></label>
+            </form>
+        </div><br><br>
+        <button type="button" class="btn btn-primary btn-lg">Large button</button>
 
         <!-- declaração de js -->
         <script src="js/jquery-3.3.1.slim.min.js"></script>
         <script src="js/popper.min.js" ></script>
-        <script src="js/bootstrap.min.js"></script>
-
+        <script src="/js/bootstrap.min.js"></script>
+    
+        <footer id="rodape">
+            <p>Copyright © 2022. Todos os direitos reservados.</p>
+        </footer>
     </body>
 </html>
